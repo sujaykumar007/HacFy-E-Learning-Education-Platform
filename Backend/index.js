74464f41f909961b92ae6643fc13355fe80bea50
Gowtham MA <magowtham7@gmail.com>
2024-11-14 23:28:59 +0530

    updated the cors config in backend changed the api in frontend form and updated the mongodb connection config

--- Backend/index.js
+++ Backend/index.js
@@ -1,29 +1,33 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
-const authRoutes = require('./routes/auth');  // Make sure this path is correct
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const authRoutes = require("./routes/auth");
+const dotEnv = require("dotenv");
 
 const app = express();
+
+//loading the env variables from .env file
+dotEnv.config();
 
 app.use(express.json());
 
 // CORS configuration
-app.use(cors({
-  origin: 'http://localhost:5177',  // Frontend URL
-  methods: 'GET,POST,PUT,DELETE',
-  credentials: true,
-}));
+app.use(
+  cors({
+    origin: "http://localhost:5173", // Frontend URL
+    methods: "GET,POST,PUT,DELETE",
+    credentials: true,
+  }),
+);
 
 // Use the auth routes for any /api/auth path
-app.use('/api/auth', authRoutes);
+app.use("/api/auth", authRoutes);
 
 // MongoDB connection
-mongoose.connect(process.env.MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
-.then(() => console.log('MongoDB connected'))
-.catch(err => console.log('MongoDB connection error:', err));
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.log("MongoDB connection error:", err));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
