a9d7ad022e373df974d62d3fbc3f3778efb464dd
abhishekrgadad <abhishekgadad485@gmail.com>
2024-11-26 19:28:18 +0530

    updated logo

--- src/components/internship.jsx
+++ src/components/internship.jsx
@@ -84,7 +84,7 @@ export default function internship() {
             {/* Logo */}
             <div className="flex items-center">
               <img
-                src="/assets/hacfy1.png"
+                src="/public/hacfy1.png"
                 alt="HacFy Logo"
                 className="h-8 w-8 mr-2"
               />
